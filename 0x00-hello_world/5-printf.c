79f2468cbb34060d60bae6b8f968e240444b89f3
philani-kubeka <blue.vale112@gmail.com>
2023-09-07 16:27:20 -0700

    update

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in printf function
+ *
+ * Description: using main function
+ * this program prints with proper grammer,but the outcome is a piece of art
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
