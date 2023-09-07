2b91fbe9f9a1ec92a0b0e3feb07305c2d294decc
philani-kubeka <blue.vale112@gmail.com>
2023-09-07 16:17:37 -0700

    updates

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
  /**
+  * main - Entry point
+  *
   * Description: main
+  *
   * this programme prints Programming is like building a multiligual puzzle
-  * Return: 0
+  *
+  * Return: Always 0
   */
 int main(void)
 {
-	/*main*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
