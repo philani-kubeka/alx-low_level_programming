c199245e6b4673a7d61f1a17df8a8296ad8a5198
philani-kubeka <blue.vale112@gmail.com>
2023-09-07 15:23:16 -0700

    introduction

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/*main - prints '"'Programming is like building a multiligual puzzle followed by return (0)*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
