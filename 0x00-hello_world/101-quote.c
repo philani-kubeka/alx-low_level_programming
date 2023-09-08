c525b25857ad54e2309d6ca45bcf332aae9a25d9
philani-kubeka <blue.vale112@gmail.com>
2023-09-07 17:20:44 -0700

    final update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - print the string in printf function
@@ -10,6 +10,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 5);
 	return (1);
 }
