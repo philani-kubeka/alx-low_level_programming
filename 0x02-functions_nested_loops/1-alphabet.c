84428f69bc9e01aefe7e8b39ebc7413565cca944
philani-kubeka <blue.vale112@gmail.com>
2023-09-12 09:59:41 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet - Prints alphabet.
+ *
+ *
+ * Return: Always 0.
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter)
+
+	putchar('\n');
+	return (0);
+}
