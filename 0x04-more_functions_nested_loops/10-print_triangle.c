89ccd6f12b18e65e1e97bd9e7086b7764d9257b7
philani-kubeka <blue.vale112@gmail.com>
2023-09-14 11:35:29 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,22 +8,24 @@
 void print_triangle(int size)
 {
 	int i, j;
+	int n = size - 1;
 
 	if (size > 0)
 	{
-		for (i = 0; i <= size; i++)
+		for (i = 0; i < size; i++)
 		{
-			for (j = size - i; j > 0; j--)
-				_putchar(' ');
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
 
-			for (j = 0; j < i; j++)
-				_putchar('#');
-
-			if (i == size)
-				continue;
-
+			n--;
 			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
