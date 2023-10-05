0a987eab09d74703535666774342ab9165ff1d25
philani-kubeka <blue.vale112@gmail.com>
2023-10-05 14:38:48 -0700

    more malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,15 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - a program that multiplies two numbers.
+ * is_positive_integer - check if a string represents a positive integer
  *
- * @argv: argument value
- * @argc: argument count
+ * @str: input string
  *
- * Return: always 0
+ * Return: 1 if it's a positive integer, 0 otherwise
  */
+int is_positive_integer(char *str)
+{
+	if (*str == '\0')
+	{
+		return (0);
+	}
 
+	while (*str)
+	{
+		if (!isdigit(*str))
+		{
+			return (0);
+		}
+		str++;
+	}
+
+	return (1);
+}
+
+/**
+ * main - a program that multiplies two positive numbers.
+ *
+ * @argc: argument count
+ * @argv: argument values
+ *
+ * Return: 0 on success, 98 on error
+ */
 int main(int argc, char **argv)
 {
 	int num1, num2, ans;
@@ -17,10 +43,16 @@ int main(int argc, char **argv)
 	if (argc != 3)
 	{
 		printf("Error\n");
-		exit (98);
+		return (98);
 	}
 	else
 	{
+		if (!is_positive_integer(argv[1]) || !is_positive_integer(argv[2]))
+		{
+			printf("Error\n");
+			return (98);
+		}
+
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		ans = num1 * num2;
